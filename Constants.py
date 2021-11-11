e1ce05ef6d013e692151c5fd13cf66c92524fac8
Jonas Blanc <jonas.blanc@epfl.ch>
2021-11-11 10:41:47 +0100

    Rename files constants

--- Constants.py
+++ Constants.py
@@ -1,5 +1,5 @@
-FILE_IN = 'quotes-2020.json.bz2' 
-FILE_QUOTES = "nps-tokens-2020.csv.bz2"
+FILE_QUOTES = 'quotes-2020.json.bz2' 
+FILE_NEWSPAPER_TOKEN = "nps-tokens-2020.csv.bz2"
 FILE_SPEAKER_NEWSPAPER = "speaker-newspaper-2020.csv.bz2"
 
 FILE_NEWSPAPER_SPEAKER_TFIDF = "newspaper_speaker_tfidf.npz"
